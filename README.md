812a5cdb00eaaa43c2c4ee41b03fb4df42a60785
Mithran B <32775475+BalaMithran@users.noreply.github.com>
2020-09-06 19:04:22 +0530

    added basic files
    version 1.0.0

---
+++ README.md
@@ -0,0 +1,12 @@
+# Chrome Extension New Tab Wallpaper
+
+This repo contains a chrome extension which you can use to display random walpapers with qoutes and animations as soon as you open a new tab.
+
+In order to use this extension, run- 
+- `git clone`
+- Open Chrome > Settins > More tools > Extensions OR open `chrome://extensions/` in chrome
+- In the Extensions page, tick Developer mode on the top righ corner.
+- Click `Load unpacked-extension`.
+- Choose the cloned repo and you are Done!
+
+Whenevr you open a new tab a beautiful random wallpaper to would show up everytime with a special qoute of the day.
---
+++ src/display.js
@@ -0,0 +1,48 @@
+// (function () {
+//     var qoute = [
+//         "They don't want us to win.",
+//         "Egg whites, turkey sausage, wheat toast, water. Of course they don't want us to eat our breakfast, so we are going to enjoy our breakfast.",
+//         "You smart, you loyal, you a genius.",
+//         "Congratulations, you played yourself.",
+//         "The key to more success is to have a lot of pillows.",
+//         "Life is what you make it, so let's make it.",
+//         "They key is to have every key, the key to open every door.",
+//         "Learning is cool, but knowing is better, and I know the key to success."
+//     ];
+
+//     var images = [
+//         "assets/images/1.jpg",
+//         "assets/images/2.jpg"        
+//     ];
+
+//     function chooseOne(arr) {
+//         return arr[Math.floor(Math.random() * arr.length)];
+//     }
+
+//     window.onload = function() {
+//         var randomImage = chooseOne(images);
+//         var randomQoute = chooseOne(qoute);
+
+//         document.getElementById('background-image')
+//         .style.backgroundImage = 'url("'+randomImage+'")' ;
+//         document.getElementById('qoute').innerHTML = randomQoute;
+//         document.getElementById('author').innerHTML = "Rakshit Midha";
+//         document.getElementById('qoute').className = 'move';
+//         document.getElementById('author').className = 'move';
+//     };
+// })();
+
+
+function myFunction() {
+    var d = new Date();
+    var n = d.getSeconds();
+    var ans=(n*100)/60;
+    var testing=ans;
+    var testing2=100-testing;
+    let root = document.documentElement;
+    root.style.setProperty('--mouse-x', testing+"%");
+    root.style.setProperty('--mouse-y', testing2+"%");
+  }
+
+  var myVar = setInterval(myFunction, 1000);
+
